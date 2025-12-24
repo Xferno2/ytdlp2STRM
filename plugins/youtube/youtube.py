959b2423a6346121bcb882c80da1950758145c33
fartwhif <fartwhif@tuta.io>
2025-12-24 14:12:48 -0600

    select by language only if at least one stream specifies language id

--- plugins/youtube/youtube.py
+++ plugins/youtube/youtube.py
@@ -608,6 +608,20 @@ def filter_and_modify_bandwidth(m3u8_content):
     high_audio = False
     sd_audio = ""
     
+    # select by language only if at least one stream specifies language id
+    sel_by_lang = False
+    if lang and lang.strip():
+        for i in range(len(lines)):
+            line = lines[i]
+            if line.startswith("#EXT-X-STREAM-INF:"):
+                info_lang = ""
+                pattern = r'YT-EXT-AUDIO-CONTENT-ID="([^."]+)'
+                match = re.search(pattern, line)
+                if match:
+                    info_lang = match.group(1)
+                    if (info_lang != ""):
+                        sel_by_lang = True
+    
     for i in range(len(lines)):
         line = lines[i]
         
@@ -616,8 +630,8 @@ def filter_and_modify_bandwidth(m3u8_content):
             url = lines[i + 1]
             bandwidth = int(info.split("BANDWIDTH=")[1].split(",")[0])
 
-            desired_lang = True
-            if lang and lang.strip():
+            desired_lang = not sel_by_lang
+            if sel_by_lang:
                 info_lang = ""
                 pattern = r'YT-EXT-AUDIO-CONTENT-ID="([^."]+)'
                 match = re.search(pattern, info)
@@ -629,7 +643,7 @@ def filter_and_modify_bandwidth(m3u8_content):
                         else:
                             desired_lang = False
 
-            if bandwidth > highest_bandwidth and (best_video_info == None or desired_lang):
+            if bandwidth > highest_bandwidth and desired_lang:
                 highest_bandwidth = bandwidth
                 best_video_info = info.replace(f"BANDWIDTH={bandwidth}", "BANDWIDTH=279001")
                 best_video_url = url
